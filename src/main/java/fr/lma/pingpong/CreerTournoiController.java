bd3319670a080f85de53851b7cc7e9a581e3e39f
MaximeVeschembes <maxime.veschembes@toulouse.miage.fr>
2023-02-02 15:42:00 +0100

    Création de tournois fonctionnelle (controlleur+vue) Suppression des classes Ville et Stade (attribut en String dans la classe Tournoi)

--- src/main/java/fr/lma/pingpong/CreerTournoiController.java
+++ src/main/java/fr/lma/pingpong/CreerTournoiController.java
@@ -1,17 +1,82 @@
 package fr.lma.pingpong;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 import java.util.ArrayList;
 
+/**
+ * Classe controlleur de creerTournoi.fxml
+ * @author MaximeVeschembes
+ */
 public class CreerTournoiController {
 
-    private String dateDebut;
-    private String dateFin;
-    private ArrayList<Match> matchs;
-    private Stade stade;
+    @FXML
+    private TextField nomTournoi;
+    @FXML
+    private DatePicker dateDebutTournoi;
+    @FXML
+    private DatePicker dateFinTournoi;
+    @FXML
+    private ToggleGroup nbJoueur;
+    @FXML
+    private RadioButton nbJoueur8;
+    @FXML
+    private RadioButton nbJoueur16;
+    @FXML
+    private RadioButton nbJoueur32;
+    @FXML
+    private ToggleGroup typeT;
+    @FXML
+    private RadioButton typeTSimple;
+    @FXML
+    private RadioButton typeTDouble;
+    @FXML
+    private TextField stade;
+    @FXML
+    private TextField ville;
 
+    /**
+     * Permet de retourner au menu principal
+     * @param actionEvent
+     */
     public void afficherSceneMenu(ActionEvent actionEvent) {
         AccueilApplication.setFXMLForStage("accueil.fxml");
+    }
+
+    /**
+     * Gestion de la création du tournois
+     * @param actionEvent
+     */
+    public void creerTournoi(ActionEvent actionEvent) {
+
+        int nbJ;
+
+        if (nbJoueur.getSelectedToggle().equals(nbJoueur8)) {
+            nbJ = 8;
+        } else if (nbJoueur.getSelectedToggle().equals(nbJoueur16)) {
+            nbJ = 16;
+        } else {
+            nbJ = 32;
+        }
+
+        if (typeT.getSelectedToggle().equals(typeTSimple)) {
+            Tournoi tournoi = new TournoiSimple(nomTournoi.getText(),
+                    dateDebutTournoi.getPromptText(),
+                    dateFinTournoi.getPromptText(),
+                    nbJ,
+                    new ArrayList<>(7),
+                    stade.getText(),
+                    ville.getText());
+        } else {
+            Tournoi tournoi = new TournoiDouble(nomTournoi.getText(),
+                    dateDebutTournoi.getPromptText(),
+                    dateFinTournoi.getPromptText(),
+                    nbJ,
+                    new ArrayList<>(7),
+                    stade.getText(),
+                    ville.getText());
+        }
     }
 }
---
+++ src/main/java/fr/lma/pingpong/TournoiDouble.java
@@ -0,0 +1,24 @@
+package fr.lma.pingpong;
+
+import java.util.ArrayList;
+
+/**
+ * Classe des tournois double (2 contre 2)
+ * @author MaximeVeschembes
+ */
+public class TournoiDouble extends Tournoi{
+
+    /**
+     * Constructeur de la classe TournoiDouble
+     * @param p_nom
+     * @param p_dateDebut
+     * @param p_dateFin
+     * @param p_nbJoueurs
+     * @param p_matchs
+     * @param p_stade
+     * @param p_ville
+     */
+    public TournoiDouble(String p_nom, String p_dateDebut, String p_dateFin, int p_nbJoueurs, ArrayList<Match> p_matchs, String p_stade, String p_ville) {
+        super(p_nom, p_dateDebut, p_dateFin, p_nbJoueurs, p_matchs, p_stade, p_ville);
+    }
+}
