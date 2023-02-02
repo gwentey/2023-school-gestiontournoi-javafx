0ca52e475e6b42c46f6cd36ca958346130e34e6c
MaximeVeschembes <maxime.veschembes@toulouse.miage.fr>
2023-02-02 12:13:57 +0100

    Ajout variable de création tournois dans le controller

--- src/main/java/fr/lma/pingpong/CreerTournoiController.java
+++ src/main/java/fr/lma/pingpong/CreerTournoiController.java
@@ -2,7 +2,14 @@ package fr.lma.pingpong;
 
 import javafx.event.ActionEvent;
 
+import java.util.ArrayList;
+
 public class CreerTournoiController {
+
+    private String dateDebut;
+    private String dateFin;
+    private ArrayList<Match> matchs;
+    private Stade stade;
 
     public void afficherSceneMenu(ActionEvent actionEvent) {
         AccueilApplication.setFXMLForStage("accueil.fxml");
