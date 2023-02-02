ae840def0222d037bf2b25e75197e1207a893869
Lucas <lucas.garlaschi@yahoo.com>
2023-02-02 15:02:42 +0100

    Maj button affichage tournoi

--- src/main/java/fr/lma/pingpong/AffichageTournoiController.java
+++ src/main/java/fr/lma/pingpong/AffichageTournoiController.java
@@ -1,5 +1,10 @@
 package fr.lma.pingpong;
 
+import javafx.event.ActionEvent;
+
 public class AffichageTournoiController {
-    
+
+    public void button_click_menu(ActionEvent actionEvent) {
+        AccueilApplication.setFXMLForStage("accueil.fxml");
+    }
 }
