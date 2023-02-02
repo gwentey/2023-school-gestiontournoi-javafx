c3c64a792ee8188296603823f19597c7a30ed33a
Lucas <lucas.garlaschi@yahoo.com>
2023-02-02 15:10:21 +0100

    Maj affichage tournoi

--- src/main/java/fr/lma/pingpong/AffichageTournoiController.java
+++ src/main/java/fr/lma/pingpong/AffichageTournoiController.java
@@ -1,10 +1,19 @@
 package fr.lma.pingpong;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 
-public class AffichageTournoiController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AffichageTournoiController implements Initializable {
 
     public void button_click_menu(ActionEvent actionEvent) {
         AccueilApplication.setFXMLForStage("accueil.fxml");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
